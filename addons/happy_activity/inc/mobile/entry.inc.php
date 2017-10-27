50a4f8c01d77dd6e40162f37690940cdfdf00e32
daichen <543577508@qq.com>
2017-10-27 12:02:25 +0800

    报名

--- addons/happy_activity/inc/mobile/entry.inc.php
+++ addons/happy_activity/inc/mobile/entry.inc.php
@@ -7,6 +7,10 @@
  */
 global $_W;
 //获取活动数据
+$activity = pdo_fetch('select * from '.tablename().'where `uniacid`=:uniacid',array(
+    ':uniacid'=>$_W['uniacid']
+));
+var_dump($activity);
 //加载视图
 
 include $this->template('entry');
