8fcf40abd55dd347bddd27b9e78c36bfeabd2971
daichen <543577508@qq.com>
2017-10-27 14:18:50 +0800

    报名

--- addons/happy_activity/site.php
+++ addons/happy_activity/site.php
@@ -49,16 +49,10 @@ class Happy_activityModuleSite extends WeModuleSite {
         load()->func('tpl');
         $pindex = max(1, intval($_GPC["page"]));
         $psize = 2;
-
-
-
         $res = pdo_fetchall("SELECT * FROM " . tablename('happy_happyactivity_join') . " 
         WHERE `uniacid`=:uniacid ORDER BY id DESC LIMIT " . ($pindex - 1) * $psize . ',' . $psize, array(':uniacid'=>$_W['uniacid']));
-
-
-        var_dump($res);
-        exit();
         $total = count($res);
+        exit();
 //        $total = pdo_fetchcolumn('SELECT COUNT(*) FROM ' . tablename('happy_happyactivity_join') . " WHERE uniacid = '{$_W['uniacid']}'");
         $pager = pagination($total, $pindex, $psize, $url = '', $context = array('before' => 0, 'after' => 0, 'ajaxcallback' => ''));
         include $this->template('ActivityLog');
