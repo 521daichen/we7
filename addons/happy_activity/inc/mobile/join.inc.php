c58e5191742ce4d8cd9ed200e38641457f08956a
daichen <543577508@qq.com>
2017-10-27 12:36:52 +0800

    报名

--- addons/happy_activity/inc/mobile/join.inc.php
+++ addons/happy_activity/inc/mobile/join.inc.php
@@ -9,7 +9,19 @@ global $_W,$_GPC;
 load()->func('tpl');
 
 if(checksubmit('submit')){
-    var_dump($_GPC);
+    $data['openid'] = $_W['openid'];
+    $data['uniacid'] = $_W['uniacid'];
+    $data['name'] = $_GPC['username'];
+    $data['sex'] = $_GPC['sex'];
+    $data['mobile'] = $_GPC['mobile'];
+    $data['pic'] = $_GPC['pic'];
+    $data['msg'] = $_GPC['msg'];
+    $res = pdo_insert("happy_happyactivity_join",$data);
+    if($res){
+        message('编辑活动成功',$this->createWebUrl('join',array()),'success');
+    }else{
+        message('保存失败','','error');
+    }
 }
 
 include $this->template('join');
