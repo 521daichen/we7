46df22341aff9653443176c38a323719e7a402e5
daichen <543577508@qq.com>
2017-10-27 12:27:28 +0800

    报名

--- addons/happy_activity/inc/mobile/join.inc.php
+++ addons/happy_activity/inc/mobile/join.inc.php
@@ -5,6 +5,11 @@
  * Date: 2017/10/27
  * Time: 下午12:12
  */
-global $_W;
+global $_W,$_GPC;
 load()->func('tpl');
+
+if(checksubmit('submit')){
+    var_dump($_GPC);
+}
+
 include $this->template('join');
